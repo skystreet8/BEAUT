dd5b5da46f555c29d86c73d8e695b5d76782c90c
guojs <gjs1999@126.com>
2024-11-28 13:59:36 +0800

    Clean up

--- data_augmentation/scripts/utils.py
+++ data_augmentation/scripts/utils.py
@@ -1,6 +1,3 @@
-import re
-
-
 def ReadFastaFile(fname: str):
     lines = open(fname).readlines()
     assert len(lines) % 2 == 0
@@ -42,37 +39,3 @@ def ReformatFastaFile(filepath, full_header=False):
             next_seq = next_seq + l
     seqs.append(next_seq)
     return headers, seqs
-
-
-def ReadPDBFile(file):
-    lines = open(file).readlines()
-    lines = [l for l in lines if l.startswith('ATOM')]
-    lines = [s[:-1] for s in lines]
-    for l in lines:
-        if not len(l) == 80:
-            raise ValueError
-    new_lines = []
-    for l in lines:
-        fields = re.split(r'\s+', l)
-        if fields[-2] == 'H':
-            continue
-        new_lines.append(l)
-    return new_lines
-
-
-def FillPocketPDB(pocket_pdbfile, pdbfile):
-    pocket_lines = open(pocket_pdbfile).readlines()
-    pocket_lines = [l[:-1] for l in pocket_lines]
-    if pocket_lines[-1].startswith('ENDMDL'):
-        pocket_lines.pop(-1)
-    pocket_residue_nums = set([re.split(r'\s+', l)[5] for l in pocket_lines])
-    pdb_lines = ReadPDBFile(pdbfile)
-    for l in pdb_lines:
-        if l in pocket_lines:
-            continue
-        else:
-            fields = re.split(r'\s+', l)
-            if fields[5] in pocket_residue_nums:
-                pocket_lines.append(l)
-    pocket_lines.sort(key=lambda l: int(re.split(r'\s+', l)[1]))
-    return pocket_lines
