b290d0df8cb31c76959ca1cf6a181e8dcce08076
guojs <gjs1999@126.com>
2025-03-13 20:00:40 +0800

    Clean up file names

--- scripts/generate_balanced_test_sets.py
+++ scripts/generate_balanced_test_sets.py
@@ -3,7 +3,7 @@ import torch
 import random
 
 
-df = pd.read_csv('../data/sequence_dataset_v3_substrate_pocket_aug_eq_len_dist.csv')
+df = pd.read_csv('../data/sequence_dataset_v3_substrate_pocket_aug.csv')
 test_df = df.query('dataset_fold_1 == "test"').copy()
 for i in range(1, 6):
     del test_df[f'dataset_fold_{i}']
@@ -15,16 +15,13 @@ selected_indexes = [pos_indexes + neg_indexes[i * num_neg_samples:(i + 1) * num_
 sub_dfs = [test_df.loc[indexes].copy() for indexes in selected_indexes]
 for i in range(1, 6):
     sub_dfs[i - 1].to_csv(f'../data/test_set_sample_{i}.csv', index=False)
-pos_embeddings = torch.load('../data/positive_seqs_v2_substrate_pocket_aug_embeddings.pt')
-neg_embeddings = torch.load('../data/negative_seqs_v2_embeddings.pt')
+embeddings = torch.load('../data/seq_embeddings_v3_substrate_pocket_aug.pt')
 for i in range(5):
     data = {}
     headers = sub_dfs[i]['header'].values.tolist()
     for k in headers:
-        if k in pos_embeddings:
-            data[k] = pos_embeddings[k]
-        elif k in neg_embeddings:
-            data[k] = neg_embeddings[k]
+        if k in embeddings:
+            data[k] = embeddings[k]
         else:
             print(k)
             raise KeyError
