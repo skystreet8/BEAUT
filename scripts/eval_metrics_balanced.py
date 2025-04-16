64a86c49c5848ac8a6fdde21524b3cdf0a25a281
guojs <gjs1999@126.com>
2025-04-16 13:41:15 +0800

    Updated scripts

--- scripts/eval_metrics_balanced.py
+++ scripts/eval_metrics_balanced.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from models import DNNPredictor
 from dataset import SequenceTestDataset
 from sklearn.metrics import average_precision_score, f1_score, matthews_corrcoef, precision_score, recall_score,\
-    confusion_matrix, fbeta_score
+    confusion_matrix, fbeta_score, auc, precision_recall_curve
 logger = logging.getLogger('Test')
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
@@ -46,38 +46,43 @@ if __name__ == '__main__':
     precs = []
     recs = []
     conf_mats = []
-    model = DNNPredictor(1280, [256, 32])
-    results = {}
-    for i in tqdm(range(1, 6), total=5):
-        test_df = pd.read_csv(f'../data/test_set_sample_{i}.csv')
-        header2label = {t[1]: t[2] for t in test_df.itertuples()}
-        test_dataset = SequenceTestDataset(f'../data/test_set_sample_{i}_embeddings.pt')
-        test_dataloader = DataLoader(test_dataset, batch_size=128)
-        model.load_state_dict(torch.load(f'../models/BEAUT_aug.pth')['model_state_dict'])
+    folds = []
+    test_set_ids = []
+    for fold in tqdm(range(1, 6), total=5):
+        model = DNNPredictor(1280, [256, 32])
+        model.load_state_dict(torch.load(f'../models/BEAUT_aug_fold_{fold}.pth')['model_state_dict'])
         model.to(DEVICE)
-        test_probs, test_predictions = predict(model, test_dataloader, threshold=threshold)
-        test_headers = test_dataset.headers
-        test_labels = [header2label[h] for h in test_headers]
-
-        pos_probs = [t[1] for t in test_probs]
-        aupr = average_precision_score(test_labels, pos_probs)
-        auprs.append(aupr)
-        test_predictions = np.array(test_predictions)
-        test_labels = np.array(test_labels)
-        rec = round(recall_score(test_labels, test_predictions), 4)
-        recs.append(rec)
-        prec = round(precision_score(test_labels, test_predictions), 4)
-        precs.append(prec)
-        f1score = round(f1_score(test_labels, test_predictions), 4)
-        f1_scores.append(f1score)
-        f2score = round(fbeta_score(test_labels, test_predictions, beta=2), 4)
-        f2_scores.append(f2score)
-        mcc = round(matthews_corrcoef(test_labels, test_predictions), 4)
-        mccs.append(mcc)
-        conf_mat = confusion_matrix(test_labels, test_predictions)
-        conf_mats.append(list(conf_mat))
+        results = {}
+        for i in range(1, 11):
+            folds.append(fold)
+            test_set_ids.append(i)
+            test_df = pd.read_csv(f'../data/test_set_sample_{i}.csv')
+            header2label = {t[1]: t[2] for t in test_df.itertuples()}
+            test_dataset = SequenceTestDataset(f'../data/test_set_sample_{i}_embeddings.pt')
+            test_dataloader = DataLoader(test_dataset, batch_size=128)
+            test_probs, test_predictions = predict(model, test_dataloader, threshold=threshold)
+            test_headers = test_dataset.headers
+            test_labels = [header2label[h] for h in test_headers]
+            pos_probs = [t[1] for t in test_probs]
+            precisions, recalls, _ = precision_recall_curve(test_labels, pos_probs)
+            aupr = round(auc(recalls, precisions), 4)
+            auprs.append(aupr)
+            test_predictions = np.array(test_predictions)
+            test_labels = np.array(test_labels)
+            rec = round(recall_score(test_labels, test_predictions), 4)
+            recs.append(rec)
+            prec = round(precision_score(test_labels, test_predictions), 4)
+            precs.append(prec)
+            f1score = round(f1_score(test_labels, test_predictions), 4)
+            f1_scores.append(f1score)
+            f2score = round(fbeta_score(test_labels, test_predictions, beta=2), 4)
+            f2_scores.append(f2score)
+            mcc = round(matthews_corrcoef(test_labels, test_predictions), 4)
+            mccs.append(mcc)
+            conf_mat = confusion_matrix(test_labels, test_predictions)
+            conf_mats.append(list(conf_mat))
+    print([round(np.mean(auprs[i * 10:(i + 1) * 10]), 4) for i in range(5)])
     df = pd.DataFrame(data=None)
-    df.index = [f'test_set_sample_{i}' for i in range(1, 6)]
-    df = df.assign(AUPR=auprs, Recall=recs, Precision=precs, F1_score=f1_scores, F2_score=f2_scores,
-                   MCC=mccs, Confusion_matrix=conf_mats)
-    df.to_csv('../data/BEAUT_aug_eval_metrics_balanced.csv', index=True)
+    df = df.assign(fold=folds, test_set_id=test_set_ids, AUPR=auprs, Recall=recs, Precision=precs, F1_score=f1_scores,
+                   F2_score=f2_scores, MCC=mccs, Confusion_matrix=conf_mats)
+    df.to_csv('../data/BEAUT_aug_eval_metrics_balanced.csv', index=False)
--- scripts/generate_balanced_test_sets.py
+++ scripts/generate_balanced_test_sets.py
@@ -2,7 +2,7 @@ import pandas as pd
 import torch
 import random
 
-
+random.seed(1058)
 df = pd.read_csv('../data/sequence_dataset_v3_substrate_pocket_aug.csv')
 test_df = df.query('dataset_fold_1 == "test"').copy()
 for i in range(1, 6):
@@ -11,12 +11,16 @@ pos_indexes = test_df.query('label == 1').index.values.tolist()
 neg_indexes = test_df.query('label == 0').index.values.tolist()
 random.shuffle(neg_indexes)
 num_neg_samples = int(5 * len(pos_indexes))
-selected_indexes = [pos_indexes + neg_indexes[i * num_neg_samples:(i + 1) * num_neg_samples] for i in range(5)]
+selected_neg_indexes = []
+for _ in range(10):
+    selected_neg_indexes.append(random.sample(neg_indexes, num_neg_samples))
+    neg_indexes = list(set(neg_indexes) - set(selected_neg_indexes[-1]))
+selected_indexes = [pos_indexes + selected_neg_indexes[i] for i in range(10)]
 sub_dfs = [test_df.loc[indexes].copy() for indexes in selected_indexes]
-for i in range(1, 6):
+for i in range(1, 11):
     sub_dfs[i - 1].to_csv(f'../data/test_set_sample_{i}.csv', index=False)
 embeddings = torch.load('../data/seq_embeddings_v3_substrate_pocket_aug.pt')
-for i in range(5):
+for i in range(10):
     data = {}
     headers = sub_dfs[i]['header'].values.tolist()
     for k in headers:
